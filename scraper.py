50a6db0b0d96ba13d3bc6aa13aa585036447592b
jasim28v-cloud <jasim28v@gmail.com>
2026-03-10 12:05:36 +0300

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -4,7 +4,7 @@ from datetime import datetime
 import re
 
 def run_news():
-    rss_url = "https://arakpop.com/feed/rss/"
+    rss_url = "https://news.google.com/rss/search?q=site:arakpop.com/rss/"
     headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36',
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
